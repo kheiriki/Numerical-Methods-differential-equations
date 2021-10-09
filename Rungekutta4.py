17dde799fa410948b9e92f4edbe579d65eab6ceb
kheiriki <87556877+kheiriki@users.noreply.github.com>
2021-10-09 08:51:06 +0330

    Add files via upload

---
+++ Rungekutta4.py
@@ -0,0 +1,42 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+x0 = int(input("Enter your X0: "))
+
+y0 = int(input("Enter your YO: "))
+
+xf = int(input("Enter your Xf: "))
+
+n = int(input("Enter your iteration number: "))
+x = np.linspace(x0,xf,n)
+def func( x,y):
+    return (x + y)
+y = np.zeros([n])
+y[0] = y0
+h= (xf-x0)/(n-1)
+
+for i in range(1, n):
+     k1= h*func(x[i-1],y0)
+     k2= h*func(x[i-1]+h/2,y0+k1/2)
+     k3= h*func(x[i-1]+h/2,y0+k2/2)
+     k4= h*func(x[i-1]+h,y0+k3)
+     y[i] = (1/6)*(k1+2*k2+2*k3+k4) + y0
+     y0 = y[i]
+     x[i]=x0
+     x0 = x0 + h
+
+
+y_true= -x-1+np.exp(x)
+
+print("x_n\t\t    y_n\t\t    y-true")
+print("-------------------")
+for i in range(n):
+	print(format(x[i],'6f'),"\t",format(y[i],'6f'),"\t",format(y_true[i],'6f'))
+#plt.plot(x,y,'b.-',x,y_true,'r-')
+plt.plot(x, y ,'b.-', x, y_true , 'r-')
+plt.grid(True)
+plt.xlabel("Value of x")
+plt.ylabel("Value of y")
+plt.title("Approximation Solution with rung kutta's Method")
+plt.show()
+
